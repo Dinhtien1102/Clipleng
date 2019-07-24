3cb94f6f39ec388abdc7035a23da3c4c11fb943c
Dinhtien1102 <dinhconganhictuk13e1@gmail.com>
2019-07-24 09:27:05 +0700

    slider

--- Clipleng_JS.js
+++ Clipleng_JS.js
@@ -1,3 +1,6 @@
-function Clip(){
-    document.getAttributeNode("src").value = "pic_bulbon.gif";
+function slider_left() {
+    document.getElementById("slider").style.left= "-1200px";
+}
+function slider_right() {
+    document.getElementById("slider").style.left= "0px";
 }
