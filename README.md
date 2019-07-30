6cb4331a76f6ba0341e4581b16aa979bfdd0a4ee
Dinhtien1102 <dinhconganhictuk13e1@gmail.com>
2019-07-30 19:06:41 +0700

    add flie clip_gola and function slider

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Clipleng
+
+### slider with js 
++ Kết hợp css và js: 
+    - Vấn đề 1: sử lí event click & tạo khối slider
+        - khối slider cho width mong muốn và xác định vị trí cho khối slider (left= 0)
+        - có 2 nút nhận sự kiện click right & left (click right slider trượt sang phải, click left trượt sang trái)
+        - function sử lí event click khi click right đưa slider đến vị trí width mong muốn & click left đưa slider về vị trí ban đầu (left= "0px")
+    - Vấn đề 2: button left & right
+        - khi click button left thì button left ẩn & button right hiện, khi click button right thì button   right ẩn & button left hiện (vấn đề này không khó)
+        - Với nhiều slider cần có nhiều class name nhưng class của các slider  có tên giống nhau, lúc này ta xác định vị trí của class để click left & right không bị lỗi   
+        - Mỗi vị trí của class tương ứng với 1 số bắt đầu từ 0 (thêm giá trị tham số vào sự kiện của function        truyền  vào tham số bên sử lí function từ đó truyền tham số vào vị trí class ví dụ:                    document.getElementsByClassName('test')[vị trí bắt đàu từ 0])
+
+    
--- gola_vid_JS.js
+++ gola_vid_JS.js
@@ -1,9 +1,33 @@
-function slider_left(id_slider) {
+function slider_left(id_slider, i) {
     document.getElementById(id_slider).style.left= "0px";
+    document.getElementsByClassName('next_left')[i].style.display = "none"
+    document.getElementsByClassName('next_right')[i].style.display = "block"
 }
-function slider_right(id_slider) {
+function slider_right(id_slider, i) {
     document.getElementById(id_slider).style.left= "-1056px";
+    document.getElementsByClassName('next_right')[i].style.display = "none"
+    document.getElementsByClassName('next_left')[i].style.display = "block"
 }
-if (document.getElementsByClassName("row").left < 100) {
-    document.getElementById("next_left").style.display = "none"
-}
+// if (document.getElementsByClassName("row").left < 100) {
+// document.getElementById("next_left").style.display = "none"
+// }
+// if ( document.getElementById("slider_1").style.left >=  0) {
+//     document.getElementById("next_left").style.display = "none"
+// } else {
+//     document.getElementById("next_left").style.display = "block"
+//     console.log("hello")
+// }
+
+// if (document.getElementById("slider_1").style.left ==0 ) {
+//     document.getElementById("next_left").style.display = "none"
+// }
+// else {
+//     document.getElementById("next_left").style.display = "block"
+// }
+
+// if (document.getElementById("next_left").style.display == "block") {
+//     document.getElementById("next_right").style.display = "none"
+// }
+// else if (document.getElementById("next_left").style.display == "none") {
+//     document.getElementById("next_right").style.display = "block"
+// }
